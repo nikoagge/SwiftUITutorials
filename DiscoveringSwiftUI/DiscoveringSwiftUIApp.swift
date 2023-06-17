9b560850040dfa2bc652f9aa5d2082f44ef1a6f7
nikoagge <nikoagge@gmail.com>
2023-06-17 12:41:40 +0300

    Initial Commit

---
+++ DiscoveringSwiftUI/DiscoveringSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiscoveringSwiftUIApp.swift
+//  DiscoveringSwiftUI
+//
+//  Created by Nikos Aggelidis on 17/6/23.
+//
+
+import SwiftUI
+
+@main
+struct DiscoveringSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
